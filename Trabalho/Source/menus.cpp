10dd79bdee4a9eeba8285c8de724afccbf6aaffa
AmandaOSilva <amandaosilva28@gmail.com>
2020-05-30 19:24:18 +0100

    Menus e merge Luisa

--- Trabalho/Source/menus.cpp
+++ Trabalho/Source/menus.cpp
@@ -1,36 +1,154 @@
+#include <winsock2.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+//#include <term.h>
+
 #include "../Header/menus.h"
+#include "../Header/Autoridade_Publica.h"
+#include "../Header/utils.h"
 
-/****************************************************************************************************************
-                                            MAIN MENU
-****************************************************************************************************************/
-void mainMenu()
-{
 
-    int option;
+//Autoridade_Publica autoridadePublica ;// todo outra solucao inves de criar uma variavel global aqui, talvez colocar o mainMenu na main
 
-    cout << endl << "**** MAIN MENU ****" << endl
-                 << "[1] " << endl
-                 << "[2] " << endl
-                 << "[3] " << endl
-                 << "[4] " << endl
-                 << "[5] " << endl
-                 << "[6] " << endl
-                 << "[7] Exit " << endl;
+void AutoridadePublicaMenu();
 
-    option = verifyOption(1, 7);
+void printAutoridadePublicaMenu() {
 
-    switch (option)
-    {
-        case 1:
+
+    cout << "=====================================================================================" << endl
+         << "                       InspectIT: Rotas de Inspecao                                  " << endl
+         << "=====================================================================================" << endl
+         << "           AGENTES ECONOMICOS            |               INSPETORES                  " << endl
+         << "=========================================|===========================================" << endl
+         << "Adicionar Agente economico         [1]   |   Adicionar inspetor                  [6] " << endl
+         << "Remover especialidade              [2]   |   Mudar Id                            [7] " << endl
+         << "Mudar horario de funcionamento     [3]   |   Mudar especialiade                  [8] " << endl
+         << "Adicionar especialidade            [4]   |   Remover inspetor                    [9] " << endl
+         << "Remover agente economico           [5]   |   Visualizar rota diaria             [10] " << endl
+         << "Exit                               [0]   |                                           " << endl;
+
+}
+
+
+void autoridadePublicaMenu() {
+    while (true) {
+        printAutoridadePublicaMenu();
+        int option;
+        option = checkOption(0, 10);
+        //screenClear();
+
+        if (option == 0)                                            //exit option
             break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 7:
-            exit(0);
-
+        switch (option) {
+            case 6:
+                //nspetor *inspetor;
+                vector<string *> vector_buffer;
+                long int nif;
+                cout << "digite o nif do inspector a ser adicionado" << endl;
+                cin >> nif;//todo verificar nif
+                string especialidade;
+                while (especialidade != "0") {
+                    cout << "digite uma especialidade do inspector [EXIT 0]" << endl;
+                    cin >> especialidade;
+                    vector_buffer.insert(vector_buffer.begin(), &especialidade);
+                    especialidade = "";
+                    //screenClear();
+                }
+                //nspetor = new Inspetor(nif, vector_buffer);
+                break;
+        }
     }
+}
 
 
+void printDenuciasMenu() {
+    cout << "=====================================================================================" << endl
+         << "                                PAGINA DE DENUCIAS                                   " << endl
+         << "=====================================================================================" << endl;
+
+}
+
+
+void denunciasMenu() {
+
+    printDenuciasMenu();
+
+    //screenClear();
+
+    //cin nome no agente economico
+    string nome;
+
+    cout << "Digite o nome agente economico [EXIT 0]: " << endl;
+
+    getline(cin, nome);
+
+    //(!autoridadePublica.check_Agente_Economico_Existe_por_nome(nome)){}//todo conferir se ao agente economico existe?
+    if (nome == "0") { return; }
+
+
+    //cin gravidade d a denuncia
+    int gravidade;
+
+    cout << "Digite a Gravidade [EXIT 0]: " << endl;
+    cout << "(Leve - 1, Moderada - 2, Grave - 3, Muito Grave - 4)" << endl;
+    gravidade = checkOption(0, 4);
+    if (gravidade == 0) return;   //abort option
+
+
+
+    //cin especialidade
+    string especialidade;
+
+    cout << "Digite a especialidade [EXIT 0]: " << endl;
+    getline(cin, especialidade); //todo checar se essa especialide existe no agenteEconomico
+    if (especialidade == "0") { return; }
+
+
+//    Denuncia *denuncia =new Denuncia(especialidade
+//    autoridadePublica.add_denucia(denuncia);
+
+
+    //screenClear();
+
+}
+
+
+void printMainMenu() {
+
+    cout << "=====================================================================================" << endl
+         << "                       InspectIT: Rotas de Inspecao                                  " << endl
+         << "=====================================================================================" << endl
+         << "           DENUNCIAS                     |          AUTORIDADE PUBLICA               " << endl
+         << "=========================================|========================================== " << endl
+         << "Fazer denuncica                    [1]   |   gerenciar autoridade publica        [2] " << endl
+         << "Exit                               [0]   |                                           " << endl;
+
+}
+
+void mainMenu() {
+    while (true) {
+        printMainMenu();
+        int option;
+        option = checkOption(0, 2);
+        //screenClear(); // n funciona
+
+        if (option == 0)                                            //exit option
+            break;
+        switch (option) {
+            case 1:
+                denunciasMenu();
+
+                break;
+
+            case 2:
+                autoridadePublicaMenu();
+                break;
+
+
+        }
+
+        //screenClear();
+    }
 }
--- Trabalho/Source/utils.cpp
+++ Trabalho/Source/utils.cpp
@@ -1,23 +1,19 @@
 #include"../Header/utils.h"
 
-int verifyOption(int option1, int option2)
-{
+int verifyOption(int option1, int option2) {
     int option;
     bool stop = false;
 
-    while (!stop)
-    {
+    while (!stop) {
         cout << "Choose an option (" << option1 << "-" << option2 << "): ";
 
         //Apenas aceita como input números entre o limite inferior e superior (inclusive)
-        if (cin >> option && option1 <= option && option2 >= option)
-        {
+        if (cin >> option && option1 <= option && option2 >= option) {
             stop = true;
             cin.ignore(1000, '\n');
         }
             //Input é recusado, e ciclo é executada de novo até ser introduzido uma opção válida
-        else
-        {
+        else {
             cout << "Invalid option, try again..." << endl << endl;
             cin.clear();
             cin.ignore(1000, '\n');
@@ -37,14 +33,14 @@ int checkOption(int min, int max) {
             if (cin.fail()) {
                 cin.clear();
                 cin.ignore(1000, '\n');
-                throw WrongInput_option("Invalid Input. Please enter an integer");
+                throw WrongInput_option("Input invalido. Escreva um inteiro");
 
             }
                 //recusado se nao esta no intervalo
             else if (input > max || input < min) {
                 cin.clear();
                 cin.ignore(1000, '\n');
-                throw WrongInput_option("Given input is not an option. Try again");
+                throw WrongInput_option("Input invalido. Escreva um inteiro");
             } else
                 return input;
         }
@@ -55,3 +51,20 @@ int checkOption(int min, int max) {
     }
 
 }
+
+void screenClear() {
+#ifdef WINDOWS
+    std::system("cls");
+#else
+    std::system("clear");
+#endif
+
+
+}
+
+void wait() {
+    cout << endl << "[PRESS ENTER]";
+    cin.ignore();
+    while (cin.get() != '\n') {};
+}
+
