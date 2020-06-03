ac7d7258f7f14f001599390e1dd9af0fca5732df
AmandaOSilva <amandaosilva28@gmail.com>
2020-06-03 19:42:26 +0100

    adicionada mais funcoes no menu autoridade

--- Trabalho/Source/menus.cpp
+++ Trabalho/Source/menus.cpp
@@ -7,7 +7,6 @@
 #include "../Header/menus.h"
 
 
-
 void printAutoridadePublicaMenu() {
 
 
@@ -18,11 +17,11 @@ void printAutoridadePublicaMenu() {
          << "=========================================|===========================================" << endl
          << "Visualizar Agentes economicos      [1]   |   Visualizar brigadas                 [7] " << endl
          << "Adicionar Agente economico         [2]   |   Adicionar brigada                   [8] " << endl
-         << "Remover especialidade              [3]   |   Mudar Id                            [9] " << endl
-         << "Mudar horario de funcionamento     [4]   |   Mudar especialiade                 [10] " << endl
-         << "Adicionar especialidade            [5]   |   Remover brigada                    [11] " << endl
-         << "Remover agente economico           [6]   |   Visualizar rota diaria             [12] " << endl
-         << "Exit                               [0]   |                                            " << endl;
+         << "Remover especialidade              [3]   |   Mudar especialiade                  [9] " << endl
+         << "Mudar horario de funcionamento     [4]   |   Remover brigada                    [10] " << endl
+         << "Adicionar especialidade            [5]   |   Visualizar rota diaria             [11] " << endl
+         << "Remover agente economico           [6]   |                                           " << endl
+         << "Exit                               [0]   |                                           " << endl;
 
 }
 
@@ -32,7 +31,8 @@ void autoridadePublicaMenu(Autoridade_Publica &autoridade) {
         int option;
         option = checkOption(0, 10);
         //screenClear();
-
+        string esp, id;
+        bool achou = false;
         if (option == 0)                                            //exit option
             break;
         switch (option) {
@@ -40,35 +40,87 @@ void autoridadePublicaMenu(Autoridade_Publica &autoridade) {
                 cout << endl << "Lista de todos os agentes economicos:" << endl;
                 autoridade.imprimirAgentesEconomicos();
                 break;
+
             case 7:
                 cout << endl << "Lista de todos as brigadas:" << endl;
                 autoridade.imprimirBrigadas();
                 break;
-            case 8:
-                int ID, hora_comeco, numero_horas_diario;
-                string especialidade;
-                cout << "Digite o ID da brigada a ser adicionada" << endl;
-                cin >> ID;
-                if (ID == 0) { return; }
-                wait();
-                cout << "Caso a brigada tenha somente uma especialidade a di digite, caso controtrio aperte 1" << endl;
-                getline(cin,especialidade);
-                if(especialidade == "0"){ return;}
-                wait();
-                cout << "Digite o horario de inicio do comercio:" << endl;
-                cin>>hora_comeco;
-                if (hora_comeco == 0) { return; }
-                wait();
-                cout << "Digite o numero de horas que o comercio permanece aberto:" << endl;
-                cin>>numero_horas_diario;
-                if (numero_horas_diario == 0) { return; }
-                wait();
-                autoridade.add_brigada(ID,especialidade,hora_comeco, numero_horas_diario);
+            case 3: {
+                achou = false;
+                while (!achou) {
+
+                    cout << "Digite o id do agente economico que deseja modificar:" << endl;
+                    cin >> id;
+                    cout << "Digite a especialidade que deseja remover:" << endl;
+                    cin >> esp;
+                    vector<string> atividades = autoridade.get_agentes().at(stoi(id) - 1)->get_atividades();
+                    vector<string>::iterator pos_atividade = find(atividades.begin(), atividades.end(), esp);
+                    if (pos_atividade != atividades.end()) {
+                        atividades.erase(pos_atividade);
+                        autoridade.get_agentes().at(stoi(id) - 1)->set_atividades(
+                                atividades);// id depende do index, n pode
+                        cout << "Especialidade removida com sucesso" << endl; // todo remover do ficheiro
+                        achou = true;
+                    } else { cout << "Este agente economico nao possue essa especialidade, tente novamente" << endl; }
+
+                }
                 break;
+
+            }
+            case 6: {
+                achou = false;
+                while (!achou) {
+                    cout << "Digite o id do agente economico que deseja remover:"
+                         << endl;// ao remover mudar os ids de todos os agentes? n serao mais por index
+                    cin >> id;
+                    vector<AgenteEconomico *> agts = autoridade.get_agentes();
+                    if (stoi(id) < agts.size()) {
+                        agts.erase(agts.begin() + stoi(id) - 1);
+                        cout << "removido com sucesso" << endl;
+                    } else {
+                        cout << " nao existe um agente com esse id" << endl;
+                    }
+                }
+                case 5: {
+                    cout << "Digite o id do agente economico que deseja modificar:" << endl;
+                    cin >> id;
+                    cout << "Digite a especialidade que deseja remover:" << endl;
+                    cin >> esp;
+                    vector<string> atividades = autoridade.get_agentes().at(stoi(id) - 1)->get_atividades();
+                    atividades.push_back(esp);
+                    cout << "Especialidade adicionada com sucesso" << endl;
+                    break;
+                }
+                case 8: {
+                    int ID, hora_comeco, numero_horas_diario;
+                    string especialidade;
+                    cout << "Digite o ID da brigada a ser adicionada" << endl;
+                    cin >> ID;
+                    if (ID == 0) { return; }
+                    wait();
+                    cout << "Caso a brigada tenha somente uma especialidade a di digite, caso controtrio aperte 1"
+                         << endl;
+                    getline(cin, especialidade);
+                    if (especialidade == "0") { return; }
+                    wait();
+                    cout << "Digite o horario de inicio do comercio:" << endl;
+                    cin >> hora_comeco;
+                    if (hora_comeco == 0) { return; }
+                    wait();
+                    cout << "Digite o numero de horas que o comercio permanece aberto:" << endl;
+                    cin >> numero_horas_diario;
+                    if (numero_horas_diario == 0) { return; }
+                    wait();
+                    autoridade.add_brigada(ID, especialidade, hora_comeco, numero_horas_diario);
+                    cout << "Brigada adicionada com sucesso" << endl;
+                    break;
+                }
+
+            }
         }
+
     }
 }
-
 
 void printDenuciasMenu() {
     cout << "=====================================================================================" << endl
