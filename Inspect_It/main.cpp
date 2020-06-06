a278e8113067a48c09f60027e81e9ff820a77e6a
Luísa Maria <42820848+Leao-marinho@users.noreply.github.com>
2020-06-06 16:35:26 +0100

    Atualizado

--- Inspect_It/main.cpp
+++ Inspect_It/main.cpp
@@ -1,8 +1,20 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+#include <iostream>
 
-int main(int argc, char* argv[]) {
+#include "src/menus.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    AutoridadePublica autoridade;
+
+    cout << "Bem vindo!" << endl;
+
+    mainMenu(autoridade);
+
     testing::InitGoogleTest(&argc, argv);
     std::cout << "CAL Lab Class 05" << std::endl;
     return RUN_ALL_TESTS();
